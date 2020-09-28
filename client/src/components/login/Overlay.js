41572c7c7e1c7946293280ce4ccf74d0acb74948
Juhee Kim <sayjh26@gmail.com>
2020-09-28 14:47:11 -0700

    added sign in page

---
+++ client/src/components/login/Overlay.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+interface Props {
+    handleClickSignUpButton(event: any): void;
+    handleClickSignInButton(event: any): void;
+}
+
+class Overlay extends Component {
+    render() {
+        const { handleClickSignUpButton, handleClickSignInButton } = this.props;
+        return (
+            <div className="overlay-container">
+                <div className="overlay">
+                    <div className="overlay-panel overlay-left">
+                        <h1>Welcome Back!</h1>
+                        <p className="overlay-description">
+                            I have an account!
+                        </p>
+                        <button
+                            className="ghost form-button"
+                            id="signIn"
+                            onClick={handleClickSignInButton}
+                        >Sign In</button>
+                    </div>
+                    <div className="overlay-panel overlay-right">
+                        <h1>Hello!</h1>
+                        <p className="overlay-description">
+                            I am new!
+                        </p>
+                        <button
+                            className="ghost form-button"
+                            id="signUp"
+                            onClick={handleClickSignUpButton}
+                        >Sign Up</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Overlay;
