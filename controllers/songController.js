50f29b3adf51dd23f90c2dbae24e38397c0efb80
Nick Guimarais <Nick.guimarais@gmail.com>
2020-09-29 15:41:27 -0700

    updating updateSong functions

--- controllers/songController.js
+++ controllers/songController.js
@@ -36,8 +36,28 @@ module.exports = {
         db.Song.find({author: req.params.author})
         .then(dbAuthor => res.json(dbAuthor))
         .catch(err => res.status(422).json(err))
-      }
+      },
 
+      updateSongTitle: function(req, res) {
+        db.Song.findOneAndUpdate({title: req.params.title}, req.body)
+        .then(dbUpdateSong => res.json(dbUpdateSong))
+      .catch(err => res.status(422).json(err));
+      },
+
+      updateSongPrice: function(req, res) {
+        db.Song.findOneAndUpdate({price: req.params.price}, req.body)
+        .then(dbUpdatePrice => res.json(dbUpdatePrice))
+      .catch(err => res.status(422).json(err));
+
+      },
+
+      updateSongAuthor: function(req, res) {
+        db.Song.findOneAndUpdate({author: req.params.author}, req.body)
+        .then(dbUpdateAuthor => res.json(dbUpdateAuthor))
+      .catch(err => res.status(422).json(err));
+
+      }
+        
 }
 
 
