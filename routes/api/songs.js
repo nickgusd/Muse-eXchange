145ef18edd934be663cd17be9ae6e9ccec9e5102
Nick Guimarais <Nick.guimarais@gmail.com>
2020-09-29 11:23:37 -0700

    updated find song queries

--- routes/api/songs.js
+++ routes/api/songs.js
@@ -9,4 +9,17 @@ router.route('/:userid')
 .get(songController.findUserSongs)
 .post(songController.AddUserSongs)
 
+router.route('/:genre')
+.get(songController, findSongsGenre)
+
+router.route('/:price')
+.get(songController, findSongsPrice)
+
+router.route('/:title')
+.get(songController, findSongsTitle)
+
+router.route('/:author')
+.get(songController, findSongsAuthor)
+
+
 module.exports = router;
