9fcfabaa56a2b565651c989b7e900293bb4fb92c
Edward Reyes <reyes.edwardf@gmail.com>
2020-10-03 04:51:29 -0700

    added music player

--- client/src/components/Music/MusicPlayer.js
+++ client/src/components/Music/MusicPlayer.js
@@ -1,112 +1,27 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Table from 'react-bootstrap/Table';
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, ListGroup, Table } from 'react-bootstrap';
+import Player from './Player';
+
 const MusicPlayer = () => {
+
+  const [song, setSong] = useState('https://res.cloudinary.com/dxp5wxx2f/raw/upload/v1601693662/MusiceXchange/Porcelain_Cover_lsu9pb.m4a');
+
   return (
     <Container fluid>
-      <Row style={{ height: "361px" }}>
-        <Col md="auto"className="p-0" style={{ height: "100%" }}>
+      <Row style={{ height: "361px" }} className='border border-secondary'>
+        <Col md="auto" className="p-0" style={{ height: "100%" }}>
           <Card className="text-left border-0" style={{ width: '18rem' }}>
             <Card.Img variant="top" src="https://res.cloudinary.com/dxp5wxx2f/image/upload/v1601671291/MusiceXchange/Screen_Shot_2020-09-27_at_1.51.31_AM_loeh6c.png" />
-            <Card.Body>
+            <Card.Body className='pb-0'>
               <Card.Title>Song Title</Card.Title>
               <Card.Text>Artist:</Card.Text>
             </Card.Body>
           </Card>
         </Col>
-        <Col className="p-0" style={{height: "100%", overflow: "auto" }}>
-          <Table striped bordered hover size="sm" variant="light" className="w-100 text-left">
-            <thead>
-              <tr>
-                <th></th>
-                <th>Song</th>
-                <th>Artist</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>4</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>5</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>6</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>7</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>8</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>9</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>10</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>11</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>12</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>13</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>14</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-              <tr>
-                <td>15</td>
-                <td>Song</td>
-                <td>Artist</td>
-              </tr>
-            </tbody>
-          </Table>
-          {/* <ListGroup className="w-100 text-left">
+        <Col className="p-0" style={{ height: "100%", overflow: "auto" }}>
+          <ListGroup className="w-100 text-left">
+            <ListGroup.Item onClick={() => setSong('https://res.cloudinary.com/dxp5wxx2f/raw/upload/v1601693662/MusiceXchange/Porcelain_Cover_lsu9pb.m4a')}>Porcelain Cover by Edward Reyes</ListGroup.Item>
+            <ListGroup.Item onClick={() => setSong('https://res.cloudinary.com/dxp5wxx2f/raw/upload/v1601694385/MusiceXchange/Air_l3mhye.m4a')}>Air by Edward Reyes</ListGroup.Item>
             <ListGroup.Item>Song: -- by --</ListGroup.Item>
             <ListGroup.Item>Song: -- by --</ListGroup.Item>
             <ListGroup.Item>Song: -- by --</ListGroup.Item>
@@ -119,13 +34,12 @@ const MusicPlayer = () => {
             <ListGroup.Item>Song: -- by --</ListGroup.Item>
             <ListGroup.Item>Song: -- by --</ListGroup.Item>
             <ListGroup.Item>Song: -- by --</ListGroup.Item>
-            <ListGroup.Item>Song: -- by --</ListGroup.Item>
-            <ListGroup.Item>Song: -- by --</ListGroup.Item>
-          </ListGroup> */}
+          </ListGroup>
         </Col>
       </Row>
-      <Row className="bg-dark text-light" style={{height: "20vh"}}>
-        <div>Media Player here</div>
+      <Row className="bg-dark text-light border border-secondary border-top-0">
+        <div style={{height: '10vh', background: "grey"}}></div>
+        <Player song={song} />
       </Row>
     </Container>
   )
