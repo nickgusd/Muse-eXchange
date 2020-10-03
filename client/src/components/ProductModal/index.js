ea2daa0d9927a85d83ab3fc2f4a191f4de288d69
Edward Reyes <reyes.edwardf@gmail.com>
2020-10-02 19:01:18 -0700

    cleaned up accountpages_demo

--- client/src/components/ProductModal/index.js
+++ client/src/components/ProductModal/index.js
@@ -1,65 +1,60 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import ModalForm from '../ModalForm'
 import API from '../../utils/API';
 
-function ProductModal({ state, open, close}) {
+function ProductModal({ state, open, close }) {
   const [user, setUser] = useState();
-  useEffect(()=> {
-    if(localStorage.getItem("currentUser")){
+  useEffect(() => {
+    if (localStorage.getItem("currentUser")) {
       setUser(JSON.parse(localStorage.getItem("currentUser")));
     }
-    
-  },[])
-    const [value, setValue] = useState({
-        select: "Hip Hop"
-    })
-    
 
-    
-    
-   
-    
+  }, [])
+  const [value, setValue] = useState({
+    select: "Hip Hop"
+  })
 
-    const handleSubmit = () => {
+
+  const handleSubmit = () => {
     //    await value
-       console.log(value.artist)
-       console.log(user._id)
-       API.AddSongs(user._id,{
+    console.log(value.artist)
+    console.log(user._id)
+    API.AddSongs(user._id, {
 
-        author:value.artist,
-        title:value.title,
-        genre:value.select,
-        price:value.price
+      author: value.artist,
+      title: value.title,
+      genre: value.select,
+      price: value.price
 
-       })
-    }
+    })
+  }
 
-    return (
-      <>
-       
-  
-        <Modal
-          show={state}
-          onHide={close}
-          backdrop="static"
-          keyboard={false}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Add New Product</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <ModalForm getValue={setValue} values={value} />
-          </Modal.Body>
-          <Modal.Footer>
+  return (
+    <>
+
+
+      <Modal
+        show={state}
+        onHide={close}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Product</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ModalForm getValue={setValue} values={value} />
+        </Modal.Body>
+        <Modal.Footer>
           <Button variant="dark" onClick={handleSubmit}>Submit</Button>
-            <Button variant="dark" onClick={close}>
-              Close
+          <Button variant="dark" onClick={close}>
+            Close
             </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    )
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
 }
 export default ProductModal
