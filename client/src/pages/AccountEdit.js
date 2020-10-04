934a4ee127729e9381ddc3924693e18b37aaffcd
Juhee Kim <sayjh26@gmail.com>
2020-10-03 22:09:31 -0700

    added UserProfile.js and changed favicon

---
+++ client/src/pages/AccountEdit.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  Container,
+  Grid,
+  makeStyles
+} from '@material-ui/core';
+import Page from '../components/Accounts/Page';
+import Profile from '../components/Accounts/Profile';
+import ProfileDetails from '../components/Accounts/ProfileDetails';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
+}));
+
+const Account = () => {
+  const classes = useStyles();
+
+  return (
+    <Page
+      className={classes.root}
+      title="Account"
+    >
+      <Container maxWidth="lg">
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xs={12}
+          >
+            <Profile />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={6}
+            xs={12}
+          >
+            <ProfileDetails />
+          </Grid>
+        </Grid>
+      </Container>
+    </Page>
+  );
+};
+
+export default Account;
